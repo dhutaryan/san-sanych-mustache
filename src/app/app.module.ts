d8cd01aed71c4fc7059ae186e8af822455bdee57
Dmitry Gutoryan <dmitry.gutoryan@gmail.com>
2022-07-29 00:17:53 +0300

    feat(app): init firestore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { TuiRootModule } from '@taiga-ui/core';
 
 import { CoreModule } from './core/core.module';
@@ -13,6 +15,17 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: process.env['FIRESTORE_API_KEY'],
+        authDomain: process.env['FIRESTORE_AUTH_DOMAIN'],
+        projectId: process.env['FIRESTORE_PROJECT_ID'],
+        storageBucket: process.env['FIRESTORE_STORAGE_BUCKET'],
+        messagingSenderId: process.env['FIRESTORE_AMESSAGING_SENDER_ID'],
+        appId: process.env['FIRESTORE_APP_ID'],
+      })
+    ),
+    provideFirestore(() => getFirestore()),
     CoreModule,
     TuiRootModule,
     AppLayoutModule,
