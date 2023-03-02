c51b443378af0cc48571784488b8735b81e40343
Dmitry Gutoryan <dmitry.gutoryan@gmail.com>
2023-03-02 21:13:16 +0300

    deploy: init firestore deploy

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="vite/client" />
 
+/// <reference types="vite/client" />
+
 interface ImportMetaEnv {
   readonly VITE_FIRESTORE_API_KEY: string;
   readonly VITE_FIRESTORE_AUTH_DOMAIN: string;
