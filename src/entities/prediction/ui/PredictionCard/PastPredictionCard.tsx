e3b56e6528aeb76ddd24a7f7ea9e17c185a1ea77
Dmitry Gutoryan <dmitry.gutoryan@gmail.com>
2023-03-04 22:32:35 +0300

    feat(predictions): change badge type for past prediction card

--- src/entities/prediction/ui/PredictionCard/PastPredictionCard.tsx
+++ src/entities/prediction/ui/PredictionCard/PastPredictionCard.tsx
@@ -1,20 +1,25 @@
-import { Badge } from 'antd';
+import { Badge, theme } from 'antd';
 import { FC } from 'react';
 
 import { PredictionCard, Props as PredictionCardProps } from './PredictionCard';
 
+const { useToken } = theme;
+
 type Props = PredictionCardProps;
 
 export const PastPredictionCard: FC<Props> = ({ prediction }) => {
+  const { token } = useToken();
+  const defaultPoint = 'N/A';
+  const score1 = prediction.team1.score || defaultPoint;
+  const score2 = prediction.team2.score || defaultPoint;
+
   return (
-    <Badge.Ribbon
-      text={
-        <>
-          {prediction.team1.score} - {prediction.team2.score}
-        </>
-      }
+    <Badge
+      color={token.colorPrimary}
+      offset={[-16, 4]}
+      count={`${score1} - ${score2}`}
     >
       <PredictionCard prediction={prediction} />
-    </Badge.Ribbon>
+    </Badge>
   );
 };
