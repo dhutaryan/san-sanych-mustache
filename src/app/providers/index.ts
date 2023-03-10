8ce5661b0f947f58ec99ff703bd811147a69fbfd
Dmitry Gutoryan <dmitry.gutoryan@gmail.com>
2023-03-10 22:41:15 +0300

    chore: rename antd hoc

--- src/app/providers/index.ts
+++ src/app/providers/index.ts
@@ -1,6 +1,6 @@
 import compose from 'compose-function';
 
-import { withAntd } from './withAtnd';
+import { withAntd } from './withAntd';
 import { withRouter } from './withRouter';
 
 export const withProviders = compose(withRouter, withAntd);
